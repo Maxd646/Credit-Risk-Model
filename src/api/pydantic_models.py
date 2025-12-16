99c8f232f57d8606d413f2464def98317cf72128
Daniel <ethiomiracle2017@gmail.com>
2025-12-16 19:37:22 +0300

    update the test and the ...

---
+++ src/api/pydantic_models.py
@@ -0,0 +1,12 @@
+"""Pydantic request/response models for API."""
+
+from pydantic import BaseModel, Field
+
+
+class Transaction(BaseModel):
+    TransactionId: str
+    Amount: float
+
+
+class PredictionResponse(BaseModel):
+    risk_probability: float = Field(..., ge=0, le=1)
